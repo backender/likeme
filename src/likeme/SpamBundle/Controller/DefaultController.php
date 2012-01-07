b1af43d6d14503a06023b823870b78a44f476d2b
marc <marc@likeme.ch>
2012-01-07 20:50:29 +0100

    Forkflow bzw. Funktionen für C/R/U/D steht nun.

--- src/likeme/SpamBundle/Controller/DefaultController.php
+++ src/likeme/SpamBundle/Controller/DefaultController.php
@@ -38,8 +38,8 @@ class DefaultController extends Controller
      			$em = $this->getDoctrine()->getEntityManager();
    				$em->persist($spam);
     			$em->flush();
-    			return new Response('Eintrag mit ID #'.$spam->getId().'<br />'.$spam->getReason());
-     			//return $this->redirect($this->generateUrl('task_success'));
+    			//return new Response('Eintrag mit ID #'.$spam->getId().'<br />'.$spam->getReason());
+     			return $this->redirect($this->generateUrl('_listSpam'));
      		}
      	}
      	return $this->render('likemeSpamBundle:Default:newSpam.html.twig', array('form' => $form->createView()));
@@ -55,6 +55,56 @@ class DefaultController extends Controller
     		throw $this->createNotFoundException('No spam found');
     	}
     	return $this->render('likemeSpamBundle:Default:listSpam.html.twig', array('spam' => $spam)); 
+    }
+    
+    public function modSpamAction($id, Request $request)
+    { 		
+    	$em = $this->getDoctrine()->getEntityManager();
+    	$spamEntry = $em->getRepository('likemeSpamBundle:Spam')->find($id);
+    	if(!$spamEntry)
+    	{
+    		throw $this->createNotFoundException('No spam found for id '.$id);
+    	}
+    	
+   		$spam = new Spam();
+   		$spam->setprofileIdFrom($spamEntry->getprofileIdFrom());
+   		$spam->setprofileIdTo($spamEntry->getprofileIdTo());
+   		$spam->setreason($spamEntry->getreason());
+   		$spam->settimestamp(new \DateTime());
+   		
+   		$form = $this->createFormBuilder($spam)
+   		//->add('profileIdFrom', 'text')
+   		//->add('profileIdTo', 'text')
+   		->add('reason', 'text')
+   		->getForm();
+   		
+   		$validator = $this->get('validator');
+   		$errors = $validator->validate($spam);
+   		
+   		if ($request->getMethod() == 'POST') {
+   			$form->bindRequest($request);
+   		
+   			if ($form->isValid()) {
+   				$var = $request->request->get('form'); 
+   				$spamEntry->setreason($var['reason']);
+   				$em->flush();
+   				return $this->redirect($this->generateUrl('_listSpam'));
+   			}
+   		}
+   		return $this->render('likemeSpamBundle:Default:modSpam.html.twig', array('form' => $form->createView(), 'id' => $id));
+    }
+    
+    public function delSpamAction($id)
+    {
+    	$em = $this->getDoctrine()->getEntityManager();
+   		$spamEntry = $em->getRepository('likemeSpamBundle:Spam')->find($id);
+   		if(!$spamEntry)
+   		{
+   			throw $this->createNotFoundException('No spam found for id '.$id);
+   		}
+   			$em->remove($spamEntry);
+    		$em->flush();
+    		return $this->redirect($this->generateUrl('_listSpam'));
     }
     
 }
