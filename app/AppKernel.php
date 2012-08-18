732f5236fe8515e84b6c5c481839faaea6b023b0
Marc Juchli <marc@mjnet.ch>
2012-08-18 13:57:24 +0200

    Implementing KnpMenuBundle Created MainMenu as a service

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
         		
         	new FOS\UserBundle\FOSUserBundle(),
         	new FOS\FacebookBundle\FOSFacebookBundle(),
+        	new Knp\Bundle\MenuBundle\KnpMenuBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Likeme/SystemBundle/Menu/MenuBuilder.php
@@ -0,0 +1,32 @@
+<?php
+namespace Likeme\SystemBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class MenuBuilder {
+	private $factory;
+	
+	/**
+	 * @param FactoryInterface $factory
+	 */
+	public function __construct(FactoryInterface $factory) {
+		$this->factory = $factory;
+	}
+
+	public function createMainMenu(Request $request) {
+		$menu = $this->factory->createItem('root');
+
+		$menu->addChild('Home', array('route' => 'home'));
+		$menu->addChild('Profil', array('route' => 'fos_user_profile_show'));
+		$menu->addChild('Registrieren', array('route' => 'fos_user_registration_register'));
+
+			$menu->addChild('Logout', array('route' => 'fos_user_security_logout'));
+			$menu->addChild('Login', array('route' => 'fos_user_security_login'));
+
+		return $menu;
+	}
+
+}
