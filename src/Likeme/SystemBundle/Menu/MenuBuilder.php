1e3d1d2dd2daa449845c92e0f98445572e880794
Marc Juchli <marc@mjnet.ch>
2012-08-20 00:31:18 +0200

    Injected security.context to MenuBuilder. (Login/Logout dynamicly now in menu)

--- src/Likeme/SystemBundle/Menu/MenuBuilder.php
+++ src/Likeme/SystemBundle/Menu/MenuBuilder.php
@@ -1,31 +1,39 @@
 <?php
 namespace Likeme\SystemBundle\Menu;
 
+use Symfony\Component\Security\Core\SecurityContextInterface;
+
 use Knp\Menu\FactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+class MenuBuilder { 
 
-class MenuBuilder {
 	private $factory;
+	
+    protected $securityContext;
 	
 	/**
 	 * @param FactoryInterface $factory
 	 */
-	public function __construct(FactoryInterface $factory) {
+	public function __construct(FactoryInterface $factory, SecurityContextInterface $securityContext) {
 		$this->factory = $factory;
+		$this->securityContext = $securityContext;
 	}
 
 	public function createMainMenu(Request $request) {
+		
 		$menu = $this->factory->createItem('root');
 
 		$menu->addChild('Home', array('route' => 'home'));
 		$menu->addChild('Profil', array('route' => 'fos_user_profile_show'));
-		$menu->addChild('Registrieren', array('route' => 'fos_user_registration_register'));
+		$menu->addChild('Registrieren',	array('route' => 'fos_user_registration_register'));
 
+		if( $this->securityContext->isGranted('IS_AUTHENTICATED_FULLY') ){
 			$menu->addChild('Logout', array('route' => 'fos_user_security_logout'));
+		} else {
 			$menu->addChild('Login', array('route' => 'fos_user_security_login'));
-
+		}
+		
 		return $menu;
 	}
 
