cf7752080286c8af3e152410fd131fc540efda89
Leowns <lee_ati@hotmail.com>
2012-08-22 14:56:01 +0200

    autoload Facebook

--- app/autoload.php
+++ app/autoload.php
@@ -14,7 +14,7 @@ $loader->registerNamespaces(array(
     'Monolog'          => __DIR__.'/../vendor/monolog/src',
     'Assetic'          => __DIR__.'/../vendor/assetic/src',
     'Metadata'         => __DIR__.'/../vendor/metadata/src',
-	
+		
 	'FOS'			   => __DIR__.'/../vendor/bundles',
 	'Knp\Bundle'	   => __DIR__.'/../vendor/bundles',
 	'Knp\Menu'   	   => __DIR__.'/../vendor/KnpMenu/src',
@@ -22,6 +22,7 @@ $loader->registerNamespaces(array(
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
     'Twig_'            => __DIR__.'/../vendor/twig/lib',
+
 ));
 
 // intl
