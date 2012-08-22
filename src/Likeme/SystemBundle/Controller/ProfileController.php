113537e3273241438b02d6ab64b1f05bce496e8b
Marc Juchli <marc@mjnet.ch>
2012-08-22 10:46:21 +0200

    Created ProfileController

---
+++ src/Likeme/SystemBundle/Controller/ProfileController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Likeme\SystemBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use FOS\UserBundle\Controller\ProfileController as BaseController;
+
+class ProfileController extends BaseController
+{
+    /**
+     * @Route("/", name="profile")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+    
+}
