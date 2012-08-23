9e85f8c699b07b25b253333013a3ba4ac2117c87
Marc Juchli <marc@mjnet.ch>
2012-08-23 17:39:32 +0200

    Added bundle specific ProfileForm Handler/Type, need to make services now.

--- src/Likeme/SystemBundle/Controller/ProfileController.php
+++ src/Likeme/SystemBundle/Controller/ProfileController.php
@@ -41,8 +41,22 @@ class ProfileController extends BaseController
     	if (!is_object($user) || !$user instanceof UserInterface) {
     		throw new AccessDeniedException('This user does not have access to this section.');
     	}
-    	
-   		return $this->container->get('templating')->renderResponse('FOSUserBundle:Profile:show.html.'.$this->container->getParameter('fos_user.template.engine'), array('user' => $user, 'active' => self::isActive()));
+    	   		
+   		//Build form anyway
+   		$form = $this->container->get('fos_user.profile.form');
+   		$formHandler = $this->container->get('fos_user.profile.form.handler');
+   		
+   		$process = $formHandler->process($user);
+   		if ($process) {
+   			$this->setFlash('fos_user_success', 'profile.flash.updated');
+   		
+   			return new RedirectResponse($this->container->get('router')->generate('fos_user_profile_show'));
+   		}
+   		
+   		return $this->container->get('templating')->renderResponse(
+   				'FOSUserBundle:Profile:show.html.'.$this->container->getParameter('fos_user.template.engine'),
+   				array('form' => $form->createView(), 'theme' => $this->container->getParameter('fos_user.template.theme'), 'user' => $user, 'active' => self::isActive())
+   		);
 
     }
     
