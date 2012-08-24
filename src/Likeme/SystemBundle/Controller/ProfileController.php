7ad7776bd6fe67a325e62745801b82514abb6d30
Marc Juchli <marc@mjnet.ch>
2012-08-24 18:09:16 +0200

    Unfortunately there's no "clean" way to extend FOS ProfileForm in 2.0 (2.1 yes) without using current-password check. Therefore I moved Profile to traditional Controller.

--- src/Likeme/SystemBundle/Controller/ProfileController.php
+++ src/Likeme/SystemBundle/Controller/ProfileController.php
@@ -1,64 +1,60 @@
 <?php
 
 namespace Likeme\SystemBundle\Controller;
+use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
 
-
+use Likeme\SystemBundle\Form\Type\ProfileFormType;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use FOS\UserBundle\Model\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use FOS\UserBundle\Controller\ProfileController as BaseController;
 
-class ProfileController extends BaseController
-{	
-	public function isActive()
-	{
+
+class ProfileController extends Controller {
+	
+	public function isActive() {
 		$user = $this->container->get('security.context')->getToken()->getUser();
 		$active = $user->getactive();
-		
+
 		return $active;
 	}
-	
-	/**
-     * @Route("/", name="profile")
-     * @Template()
-     */
-    public function indexAction()
-    {
-        return array();
-    }
-    
-    
-    /**
-     * Show the user
-     */
-    public function showAction()
-    {
-    	$user = $this->container->get('security.context')->getToken()->getUser();
-    	
-    	if (!is_object($user) || !$user instanceof UserInterface) {
-    		throw new AccessDeniedException('This user does not have access to this section.');
-    	}
-    	   		
-   		//Build form anyway
-   		$form = $this->container->get('fos_user.profile.form');
-   		$formHandler = $this->container->get('fos_user.profile.form.handler');
-   		
-   		$process = $formHandler->process($user);
-   		if ($process) {
-   			$this->setFlash('fos_user_success', 'profile.flash.updated');
-   		
-   			return new RedirectResponse($this->container->get('router')->generate('fos_user_profile_show'));
-   		}
-   		
-   		return $this->container->get('templating')->renderResponse(
-   				'FOSUserBundle:Profile:show.html.'.$this->container->getParameter('fos_user.template.engine'),
-   				array('form' => $form->createView(), 'theme' => $this->container->getParameter('fos_user.template.theme'), 'user' => $user, 'active' => self::isActive())
-   		);
 
-    }
-    
+	/**
+	 * @Route("/profile", name="profile")
+	 * @Template()
+	 */
+	public function showAction() {
+		$user = $this->container->get('security.context')->getToken()->getUser();
+
+		if (!is_object($user) || !$user instanceof UserInterface) {
+			throw new AccessDeniedException('This user does not have access to this section.');
+		}
+
+		//Build form anyway
+		$form = $this->container->get('fos_user.profile.form');
+		$formHandler = $this->container->get('fos_user.profile.form.handler');
+
+		$process = $formHandler->process($user);
+		if ($process) {
+			$this->setFlash('fos_user_success', 'profile.flash.updated');
+
+			return new RedirectResponse($this->container->get('router')->generate('fos_user_profile_show'));
+		}
+
+		$em = $this->getDoctrine()->getEntityManager();
+		$entity = $em->getRepository('LikemeSystemBundle:User')->findOneByUsername($user);
+		$profileForm = $this->createForm(new ProfileFormType(), $entity);
+
+		return $this->container->get('templating')->renderResponse('FOSUserBundle:Profile:show.html.'. $this->container->getParameter('fos_user.template.engine'),
+				array('form' => $profileForm->createView(),	'theme' => $this->container->getParameter('fos_user.template.theme'), 'user' => $user, 'active' => self::isActive())
+				);
+
+	}
+	
+	public function editAction() {
+		return array();
+	}
 
 }
--- src/Likeme/SystemBundle/Form/Type/ProfileFormType.php
+++ src/Likeme/SystemBundle/Form/Type/ProfileFormType.php
@@ -2,10 +2,10 @@
 
 namespace Likeme\SystemBundle\Form\Type;
 
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
-use FOS\UserBundle\Form\Type\ProfileFormType as BaseType;
 
-class ProfileFormType extends BaseType
+class ProfileFormType extends AbstractType
 {
 
     public function getName()
@@ -13,8 +13,8 @@ class ProfileFormType extends BaseType
         return 'likeme_user_profile';
     }
 
-    protected function buildUserForm(FormBuilder $builder, array $options)
-    {
+	public function buildForm(FormBuilder $builder, array $options)
+        {
         $builder
         ->add('location')
 		->add('aboutme')
