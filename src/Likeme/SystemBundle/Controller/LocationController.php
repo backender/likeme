b743efa4613574f4b65a9a33b7ed11271df11c50
Marc Juchli <marc@mjnet.ch>
2012-08-28 13:52:34 +0200

    LocationController & JS Widget for getting Location from database

---
+++ src/Likeme/SystemBundle/Controller/LocationController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Likeme\SystemBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class LocationController extends Controller {
+	
+
+
+	/**
+	 * @Route("/location/getlocation/{startlocation}", name="getlocation")
+	 */
+	public function placeCompleteAction($startlocation) {
+		
+		$em = $this->container->get('doctrine')->getEntityManager();
+
+		// Get saved profile pictures
+		$query = $em->createQueryBuilder()
+		->from('Likeme\SystemBundle\Entity\Location', 'l')
+		->select("l.id, l.postalcode, l.placename, l.statecode")
+		->where("l.placename like :startlocation")
+		->setParameter('startlocation', $startlocation.'%');
+		;
+		
+		$location_complete = $query->getQuery()->getResult();
+		
+		return new Response(json_encode($location_complete));
+	
+	}
+	
+	
+	/**
+	 * @Route("/location/getlocationbyid/{id}", name="getlocationbyid")
+	 */
+	public function locationByIdAction($id) {
+	
+		$em = $this->container->get('doctrine')->getEntityManager();
+	
+		// Get saved profile pictures
+		$query = $em->createQueryBuilder()
+		->from('Likeme\SystemBundle\Entity\Location', 'l')
+		->select("l.id, l.postalcode, l.placename, l.statecode")
+		->where("l.id = :id")
+		->setParameter('id', $id);
+		;
+	
+		$location_complete = $query->getQuery()->getResult();
+	
+		return new Response(json_encode($location_complete));
+	
+	}
+
+}
---
+++ src/Likeme/SystemBundle/Resources/public/widgets/location.js
@@ -0,0 +1,74 @@
+;(function ( $, window, document, undefined ) {
+
+
+$.widget( "likeme.locationcomplete",  {
+	
+	_create: function() {
+		
+		var self = this,
+			select = this.element.hide(),
+			selected = select.children( ":selected" ), value = selected.val() ? selected.text() : "";
+			
+			//set name when id is already set
+			this.selectitem(self.element.val());
+			
+			var input = this.input = $( "<input>" )
+							.attr("id", "show_location")
+			                .insertAfter( select )
+			                .val( value )
+			                .autocomplete({
+			  
+			source: function( request, response ) {
+				$.ajax({
+					/*TODO: Get dynamic link */
+					url: "http://likeme.ch/~marc/likeme/web/app_dev.php/location/getlocation/"+ request.term,
+					dataType: "json",
+					data: {
+					
+					},
+					success: function( data ) {
+						response( $.map( data, function( item ) {
+							return {
+								label: item.postalcode + " " + item.placename + ", " + item.statecode,
+								value: item.postalcode + " " + item.placename + ", " + item.statecode,
+								id: item.id
+							}
+						}));
+					}
+				});
+			},
+			select: function( event, ui ) {
+					self.element.val(ui.item.id);
+				
+			}
+	 
+		} )
+			 
+	},
+	selectitem: function( id ) {
+		var self = this;
+		$.ajax({
+			/*TODO: Get dynamic link */
+			url: "http://likeme.ch/~marc/likeme/web/app_dev.php/location/getlocationbyid/"+ id,
+			dataType: "json",
+			data: {
+			
+			},
+			success: function( data ) {
+				$.map( data, function( item ) {
+						//self.element.val(item.postalcode + " " + item.placename + ", " + item.statecode);
+						$("#show_location").val(item.postalcode + " " + item.placename + ", " + item.statecode);
+						/*label: item.postalcode + " " + item.placename + ", " + item.statecode,
+						value: item.postalcode + " " + item.placename + ", " + item.statecode,
+						id: item.id*/
+				});
+			}
+		});
+	}
+	
+});
+
+$('#likeme_user_profile_location').locationcomplete(); 	
+
+
+})( jQuery, window, document );
