2d6ed38f5357f92f1e0830c45904c4040695a46b
Marc Juchli <marc@mjnet.ch>
2012-08-30 17:28:45 +0200

    Don't need template here

--- src/Likeme/SystemBundle/Controller/LocationController.php
+++ src/Likeme/SystemBundle/Controller/LocationController.php
@@ -6,7 +6,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class LocationController extends Controller {
 	
