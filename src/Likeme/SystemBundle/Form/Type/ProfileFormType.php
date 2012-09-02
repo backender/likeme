5c040374bb6ceddd6a694d975611c27dabc0d21a
Marc Juchli <marc@mjnet.ch>
2012-09-02 18:15:11 +0200

    Created Prefs in Usertable and updated FormType, Show There's an error left that will be fixed next commits: The CSRF token is invalid. Please try to resubmit the form

--- src/Likeme/SystemBundle/Form/Type/ProfileFormType.php
+++ src/Likeme/SystemBundle/Form/Type/ProfileFormType.php
@@ -15,9 +15,26 @@ class ProfileFormType extends AbstractType
 
 	public function buildForm(FormBuilder $builder, array $options)
 	{
+		$gender_choices = array(
+				0 => 'Beide',
+				1 => 'Männlich',
+				2 => 'Weiblich'
+		);
+		
+		$age_range_choices = array(
+				'0'		=> 'Beliebiges Alter',
+				'0-15'	=> '0-15',
+				'16-20' => '16-20',
+				'21-30' => '21-30',
+				'30-45' => '30-45',
+				'46+'	=> '46+'
+		);
+		
         $builder
         ->add('location', 'text', array('label' => 'Wohnort'))
 		->add('aboutme', 'textarea', array('label' => 'Über Mich'))
+		->add('pref_gender', 'choice', array('choices' => $gender_choices))
+		->add('pref_age_range', 'choice', array('choices' => $age_range_choices))
         ;
     }
 }
