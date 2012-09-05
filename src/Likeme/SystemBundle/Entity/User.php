29742ecb6982d2f94f552720a2155034386811ce
Leowns <lee_ati@hotmail.com>
2012-09-05 18:09:33 +0200

    UserService for random users (Home)

--- src/Likeme/SystemBundle/Entity/User.php
+++ src/Likeme/SystemBundle/Entity/User.php
@@ -66,12 +66,11 @@ class User extends BaseUser
 	/**
 	 * @var integer $location
 	 *
-	 * @ORM\Column(name="location", type="integer", nullable=true)
+	 * 
 	 * @Assert\NotNull(message="Please enter location.")
-	 * @ORM\ManyToOne(targetEntity="Location")
-	 * @ORM\JoinColumns({
-	 *   @ORM\JoinColumn(name="location", referencedColumnName="id")
-	 * })
+	 * @ORM\ManyToOne(targetEntity="Location", inversedBy="id")
+	 * @ORM\JoinColumn(name="location", referencedColumnName="id", onDelete="persist")
+	 * 
 	 */
 	private $location;
 	
@@ -111,7 +110,7 @@ class User extends BaseUser
 		}
 
 	}
-	
+		
 	public function serialize()
 	{
 		return serialize(array($this->facebookID, parent::serialize()));
