adf78b4aa18d6f3257d4efd9fa3707e8b670671f
Marc Juchli <marc@mjnet.ch>
2012-09-06 22:58:06 +0200

    First statement for strange random (see todo in userservice)

--- src/Likeme/SystemBundle/Controller/HomeController.php
+++ src/Likeme/SystemBundle/Controller/HomeController.php
@@ -41,6 +41,9 @@ class HomeController extends Controller
     	
     	// UserService abrufen  	 
     	$UserService = $this->container->get('likeme.user.userservice');
+    	
+    	// Strange statement 1
+    	$strangers = $UserService->getUserMandatory();
     	 
     	// Get random user for current user ($curUser)
     	$rndUser = $UserService->getUserInRadius($user);
--- src/Likeme/SystemBundle/Extension/UserService.php
+++ src/Likeme/SystemBundle/Extension/UserService.php
@@ -1,6 +1,12 @@
 <?php
 namespace Likeme\SystemBundle\Extension;
 
+use Doctrine\Tests\Common\Annotations\Null;
+
+use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
+
+use Doctrine\Tests\Common\Annotations\False;
+use FOS\UserBundle\Model\UserInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 
@@ -29,6 +35,23 @@ class UserService implements ContainerAwareInterface
 		return $gender;
 	}
 	
+	/**
+	 * Gives the users prefered age range of strangers
+	 * 
+	 * @param string $prefAgeRange
+	 * @return array|Null
+	 */
+	public function getPrefAgeRange($prefAgeRange)
+	{
+		if (!$prefAgeRange) {
+			return null;
+		}
+		
+		$prefAgeRange = explode("-", $prefAgeRange);
+		
+		return $prefAgeRange;
+	}
+	
 	public function getRandomUser($user) {
 		
 		// Settings
@@ -49,7 +72,7 @@ class UserService implements ContainerAwareInterface
 		$prefGender = self::getPrefGender($user->getPrefGender());
 		
 		// Get prefered age range from
-		$prefAgeRange = explode("-", $user->getPrefAgeRange());
+		$prefAgeRange = self::getPrefAgeRange($user->getPrefAgeRange());
 		$prefMinAge = $prefAgeRange[0];
 		$prefMaxAge = $prefAgeRange[1];
 		
@@ -133,5 +156,56 @@ class UserService implements ContainerAwareInterface
 		
 		return false;
 	
+	}
+	
+	
+	/**
+	 * First statement (StrangerAbfrage.mm) and will exclude unwanted users in random.
+	 * Further statements will be based on this return.
+	 * 
+	 * @return array|Null
+	 * @throws AccessDeniedException if user is not found.
+	 */
+	public function getUserMandatory() {
+		
+		$user = $this->container->get('security.context')->getToken()->getUser();
+		
+		if (!is_object($user) || !$user instanceof UserInterface) {
+			throw new AccessDeniedException('This user does not have access to this section.');
+		}
+		
+		$em = $this->container->get('doctrine')->getEntityManager();
+		
+		// Get users preferences
+		$prefAgeRange = self::getPrefAgeRange($user->getPrefAgeRange());
+		$prefGender = self::getPrefGender($user->getPrefGender());
+		
+		// Build query according statement 1 (StrangerAbfrage.mm)
+		$query = $em->createQueryBuilder()
+		->select('u')
+		->from('Likeme\SystemBundle\Entity\User', 'u')
+		->where("u.id != :myid AND (u.birthday >= :minagerange OR u.birthday <= :maxagerange)")
+		->setParameter('myid', $user->getID())
+		->setParameter('minagerange', $prefAgeRange[0])
+		->setParameter('maxagerange', $prefAgeRange[1]);
+		
+		if ($prefGender != 'both') {
+			$query->andWhere("u.gender = :prefgender")
+			->setParameter('prefgender', $prefGender);
+		}
+		
+		// TODO: where != ( * like)
+		// TODO: where != ( * next)  
+		
+		$array = $query->getQuery()->getResult();
+		
+		// Testing
+		echo "Statement 1: <br />";
+		foreach($array as $record) {
+			echo "-".$record->getFirstname();
+		}
+		echo "<br />";
+		
+		return $array;
 	}
 }
