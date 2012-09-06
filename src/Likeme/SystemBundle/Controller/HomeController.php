095ebabb6baa99231d8d4ce38cafe38b25bb2b4c
Leowns <lee_ati@hotmail.com>
2012-09-06 16:58:47 +0200

    Implemented search variable amount of user in radius

--- src/Likeme/SystemBundle/Controller/HomeController.php
+++ src/Likeme/SystemBundle/Controller/HomeController.php
@@ -43,7 +43,7 @@ class HomeController extends Controller
     	$UserService = $this->container->get('likeme.user.userservice');
     	 
     	// Get random user for current user ($curUser)
-    	$rndUser = $UserService->getRandomUser($user);
+    	$rndUser = $UserService->getUserInRadius($user);
     	
     	return array('query'=>$rndUser);
     }
