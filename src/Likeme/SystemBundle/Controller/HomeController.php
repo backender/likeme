bddabae96f072def21c25cd0f0473e51ff1f48bb
Brian Mc Alister <brian@mcalister.ch>
2012-10-31 22:41:01 +0100

    added quick homepage
    Updated controller to show a view for non logged in users
    added new view file
    added some content to test
    
    not finished

--- src/Likeme/SystemBundle/Controller/HomeController.php
+++ src/Likeme/SystemBundle/Controller/HomeController.php
@@ -30,7 +30,7 @@ class HomeController extends Controller
     		return $response;
     	}
     	
-        return array();
+        return $this->render('LikemeSystemBundle:Home:start.html.twig');
     }
     
     
